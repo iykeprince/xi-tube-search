3da951c20b9c57eef254636b0f813699a800048b
Oluwaseyi Salami <osalami09@gmail.com>
2025-03-21 18:32:52 +0300

    tts update

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -19,13 +19,13 @@ export default function RootLayout({
           <div className="relative min-h-screen">
             {children}
 
-            <div className="fixed bottom-4 right-4">
+            {/* <div className="fixed bottom-4 right-4">
               <Link href="/settings">
                 <button className="bg-slate-200 dark:bg-slate-800 hover:bg-slate-300 dark:hover:bg-slate-700 p-2 rounded-full transition-colors">
                   <Settings className="h-5 w-5 text-slate-700 dark:text-slate-300" />
                 </button>
               </Link>
-            </div>
+            </div> */}
           </div>
         </ThemeProvider>
       </body>
